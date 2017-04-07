97b4eee66d4efbd099c7f0e070b70cc46a66419e
kosta <kosta@kosta-HP.mshome.net>
2017-04-07 19:43:48 +0900

    first commit

---
+++ KostaScm/src/main/java/com/example/KostaScmApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KostaScmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KostaScmApplication.class, args);
+	}
+}
